d93171dcc2281cdb7274cf2a4ca0a58e37f7b4a4
Gabr1elon3 <gabricap7@gmail.com>
2025-03-20 20:55:53 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./App.css";
-
+import React from "react";
+import LikeButton from "./components/LikeButton";
+import Counter from "./components/Counter";
+import ClickablePic from "./components/Clickablepic";
+import Dice from "./components/Dice";
 function App() {
   return (
     <div className="App">
       <h1> LAB | React Training</h1>
+      <LikeButton />
+      <Counter />
+      <ClickablePic />
+      <Dice />
     </div>
   );
 }
